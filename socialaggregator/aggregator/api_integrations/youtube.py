beae54f943a328a333eb9efd790884cfb37acb23
Matthew Dunn <matt@dubaidigitalacademy.ae>
2023-09-15 02:26:22 +0400

    Commit at end of day

--- socialaggregator/aggregator/api_integrations/youtube.py
+++ socialaggregator/aggregator/api_integrations/youtube.py
@@ -92,69 +92,35 @@ class YouTubeSearch():
         return organisedSearchResults
 
 
-searcherObject = YouTubeSearch()
+# USAGE EXAMPLE:
 
-full_results = searcherObject.getOrganisedSearchResults("Tom Pemberton")
+# searcherObject = YouTubeSearch()
 
-videoList = full_results[0]
-channelList = full_results[1]
-playlistList = full_results[2]
+# full_results = searcherObject.getOrganisedSearchResults("Tom Pemberton")
 
-print("VIDEOS:\n")
+# videoList = full_results[0]
+# channelList = full_results[1]
+# playlistList = full_results[2]
 
-for video in videoList:
-    print('%s, %s (%s)\n' % (video[0], video[3], video[4]))
-    print('%s\n' % video[1])
-    print('%s\n\n' % video[2])
+# print("VIDEOS:\n")
 
-print("CHANNELS:\n")
+# for video in videoList:
+#     print('%s, %s (%s)\n' % (video[0], video[3], video[4]))
+#     print('%s\n' % video[1])
+#     print('%s\n\n' % video[2])
 
-for channel in channelList:
-    print('%s, %s (%s)\n' % (channel[0], channel[3], channel[4]))
-    print('%s\n' % channel[1])
-    print('%s\n\n' % channel[2])
+# print("CHANNELS:\n")
 
-print("PLAYLISTS:\n")
+# for channel in channelList:
+#     print('%s, %s (%s)\n' % (channel[0], channel[3], channel[4]))
+#     print('%s\n' % channel[1])
+#     print('%s\n\n' % channel[2])
 
-for playlist in playlistList:
-    print('%s, %s (%s)\n' % (playlist[0], playlist[3], playlist[4]))
-    print('%s\n' % playlist[1])
-    print('%s\n\n' % playlist[2])
+# print("PLAYLISTS:\n")
 
+# for playlist in playlistList:
+#     print('%s, %s (%s)\n' % (playlist[0], playlist[3], playlist[4]))
+#     print('%s\n' % playlist[1])
+#     print('%s\n\n' % playlist[2])
 
-
-
-# def youtube_search(options):
-#     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-
-#     search_response = youtube.search().list(
-#         q = options.q,
-#         part='id, snippet',
-#         maxResults = options.max_results
-#     ).execute()
-
-#     videos = []
-#     channels = []
-#     playlists = []
-
-#     for search_result in search_response.get('items', []):
-#         if search_result['id']['kind'] == 'youtube#video':
-#             videos.append('%s (%s)' % (search_result['snippet']['title'], search_result['id']['videoId']))
-#         elif search_result['id']['kind'] == 'youtube#channel':
-#             channels.append('%s (%s)' % (search_result['snippet']['title'], search_result['id']['channelId']))
-#         elif search_result['id']['kind'] == 'youtube#playlist':
-#             playlists.append('%s (%s)' % (search_result['snippet']['title'], search_result['id']['playlistId']))
-
-#     print ('Videos:\n', '\n'.join(videos), '\n')
-#     print ('Channels:\n', '\n'.join(channels), '\n')
-#     print ('Playlists:\n', '\n'.join(playlists), '\n')
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--q', help='Search term', default='Google')
-# parser.add_argument('--max-results', help='Max results', default=25)
-# args = parser.parse_args()
-
-# try:
-#     youtube_search(args)
-# except HttpError as e:
-#     print('An HTTP error %d occured:\n%s' % (e.resp.status, e.content))
+# print("COMPLETE")
